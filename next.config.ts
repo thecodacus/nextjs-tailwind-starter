5412c860ac4188a5e2d9c912e0193e28326dc0f3
Anirban Kar <thecodacus@gmail.com>
2025-06-05 22:13:03 +0530

    updated Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: 'dist',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
